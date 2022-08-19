aaf473419d783b7e1d37577f1b006e96b4647e6a
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-08-19 20:58:01 +0100

    housekeeping

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container fluid>
+    <div id="footerWrap">
+      <header id="footer">
+        <p>tetett</p>
+      </header>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "siteFooter",
+}
+</script>
+
+<!-- "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#footerWrap {
+  margin-top: 0px;
+}
+
+p {
+  color: blue
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="headerWrap" style="margin-top: 0px;">
+      <header id="header">
+        <a href="#">link</a>
+      </header>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "siteHeader",
+}
+</script>
+
+<!-- "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#headerWrap {
+  background-image: none;
+  background-color: rgb(44, 48, 50);
+  border-bottom-color: rgba(140, 130, 115, 0.1);
+  width: 100%;
+  min-height: 55px;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  transition: all .3s ease-in-out;
+  box-sizing: border-box;
+  position: fixed;
+}
+
+a {
+  color: red
+}
+</style>
