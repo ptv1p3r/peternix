7e5d5c4be7c1a4aaa2e9c384bf01cf2fa6d3e81b
Pedro Roldan DEVOTEAM <PedroRoldan.DEVOTEAM@edp.com>
2022-08-19 21:23:27 +0100

    housekeeping

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <b-container fluid="sm">
+    <div id="pageWrap" :style="{ 'background-image': 'url(' + require('../src/assets/intro-bg.jpg') + ')' }">
+      <site-header></site-header>
+      100% wide until small breakpoint
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <site-footer></site-footer>
+    </div>
+  </b-container>
+  <!--  <b-container fluid="md">-->
+  <!--    100% wide until medium breakpoint-->
+  <!--  </b-container>-->
+  <!--  <b-container fluid="lg">-->
+  <!--    100% wide until large breakpoint-->
+  <!--  </b-container>-->
+  <!--  <b-container fluid="xl">-->
+  <!--    100% wide until extra large breakpoint-->
+  <!--  </b-container>-->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import siteHeader from './components/Header.vue'
+import siteFooter from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    siteHeader,
+    HelloWorld,
+    siteFooter
   }
 }
 </script>
@@ -21,6 +41,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+
+body {
+  display: block;
+  text-size-adjust: 100%;
+  word-wrap: break-word;
+  color: rgb(168, 160, 149);
+  margin: 0;
+  padding: 0;
+  border-color: #736b5e;
+}
+
+#pageWrap {
+  box-sizing: border-box;
+  padding-top: 73px;
 }
 </style>
